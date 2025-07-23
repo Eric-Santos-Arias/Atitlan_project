382b5a0e76332263172dbe5fbcc12c2b2b54f8a8
ericsantos <eric@gmail.com>
2025-07-23 11:10:15 -0600

    all set

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ container.addEventListener('scroll', () => {
   const scroll = container.scrollTop;
   const maxScroll = container.scrollHeight;
   const progress = Math.min(scroll / maxScroll, 1)
-  element.style.opacity = `${progress * 2}`;
+  element.style.opacity = `${progress * 4}`;
   title.style.opacity = `${1 - progress * 2}`;
 })
